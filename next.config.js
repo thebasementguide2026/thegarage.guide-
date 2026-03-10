33019055cbe99f9c6e98220f8f0a66f18c8062c6
thebasementguide2026 <pnason4@gmail.com>
2026-03-10 18:26:53 -0400

    Add 301 redirect from /articles/ to /guides/ for garage organization guide

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/articles/complete-garage-organization-guide',
+        destination: '/guides/complete-garage-organization-guide',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
