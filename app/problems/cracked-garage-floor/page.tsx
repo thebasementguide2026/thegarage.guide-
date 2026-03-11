1ebb6d46ea637bbe3a0661a4ad0f29d47a4feb05
thebasementguide2026 <pnason4@gmail.com>
2026-03-10 21:28:21 -0400

    Fix script tags and add closing fragment

--- app/problems/cracked-garage-floor/page.tsx
+++ app/problems/cracked-garage-floor/page.tsx
@@ -76,7 +76,7 @@ export default function CrackedGarageFloor() {
       <script type='application/ld+json' dangerouslySetInnerHTML={{ __html: JSON.stringify(repairCostSchema) }} />
       <script type='application/ld+json' dangerouslySetInnerHTML={{ __html: JSON.stringify(diagnosticSchema) }} />
       <script type='application/ld+json' dangerouslySetInnerHTML={{ __html: JSON.stringify(articleSchema) }} />
-
+<main
       {/* Hero Section */}
       <div className='relative w-full' style={{ minHeight: '420px' }}>
         <Image src='/images/crackedgarage.jpg' alt='Cracked concrete garage floor showing various crack types' fill className='object-cover' priority />
@@ -307,6 +307,7 @@ export default function CrackedGarageFloor() {
 
       </div>
     </main>
+        </>
   )
 }
 
