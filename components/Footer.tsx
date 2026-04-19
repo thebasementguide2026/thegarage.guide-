f472918b60fd8061cf3fb52f270be236844655f8
thebasementguide2026 <pnason4@gmail.com>
2026-04-19 14:13:48 +0000

    Add smart-garage-door-opener-retrofit to Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -17,6 +17,7 @@ const footerSections = [
       { label: 'Maintenance Checklist', href: '/guides/garage-maintenance-checklist' },
       { label: 'Paint a Garage Door', href: '/guides/how-to-paint-a-garage-door' },
       { label: 'Detached Garage Electrical', href: '/guides/detached-garage-electrical' },
+      { label: 'Smart Garage Retrofit', href: '/guides/smart-garage-door-opener-retrofit' },
     ],
   },
   {
