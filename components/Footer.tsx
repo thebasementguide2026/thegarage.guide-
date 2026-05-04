2368d9f09627fe3cb2cf9ce9aea5cc351d52f240
thebasementguide2026 <pnason4@gmail.com>
2026-05-04 23:37:54 +0000

    Add Door Cost Calculator to footer COST GUIDES section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -28,6 +28,7 @@ const footerSections = [
       { label: 'Epoxy Floor Cost', href: '/cost-guides/epoxy-garage-floor-cost' },
       { label: 'Conversion Cost', href: '/cost-guides/garage-conversion-cost' },
       { label: 'Door Replacement Cost', href: '/cost-guides/garage-door-replacement-cost' },
+      { label: 'Door Cost Calculator', href: '/cost-guides/garage-door-replacement-cost-calculator' },
       { label: 'Garage Addition Cost', href: '/cost-guides/garage-addition-cost' },
       { label: 'Insulation Cost', href: '/cost-guides/garage-insulation-cost' },
       { label: 'NEMA 14-50 Outlet', href: '/cost-guides/nema-14-50-outlet-installation-cost' },
